40812f1872140cc723f73d6fbc9884658f289d02
siddu1-34 <109212269+siddu1-34@users.noreply.github.com>
2024-10-10 00:58:12 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/youtube/',  // Change 'your-repo-name' to your actual GitHub repository name
+  plugins: [react()],  // Change 'your-repo-name' to your actual GitHub repository name
 })
